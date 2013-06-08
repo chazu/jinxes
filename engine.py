cbb951ccb7f27bd6854a2ba08b5f2b9861e44d7e
Chaz Straney <chaz@jackrussellsoftware.com>
2013-06-07 20:32:17 -0400

    specifies method, add class for display and widget, start modularizing initialization and drawing

--- engine.py
+++ engine.py
@@ -6,38 +6,59 @@ from caca.display import Display, DisplayError, Event
 
 import json, os
 
+class TartanDisplay(object):
+
+    def __init__(self):
+        self.canvas = Canvas(0, 0)
+        self.display = Display(self.canvas)
+        self.display.set_driver('gl')
+
+    def refresh(self):
+        self.display.refresh()
+
+
+class Widget(object):
+
+    def specifies(self, spec, key, value=None):
+        """
+        True if the spec doc has the key.
+        If value is passed in, True only
+        if key is present and equal to value
+        """
+        return key in spec.keys() and (
+            spec[key] == value if value != None else True)
+
+    def update(self):
+        pass
+
+    def border_builder(self, spec):
+        print str(spec["border"])
+        if self.specifies(spec, "border", True):
+            print "drawing border"
+            self.border = True
+            self.canvas.draw_box(0, 0, spec["width"],
+                                 spec["height"],"X")
+
+    def __init__(self, spec):
+        if "width" in spec.keys() and "height" in spec.keys():
+            print "creating canvas of specified size"
+            self.canvas = Canvas(spec["width"],
+                            spec["height"])
+            self.canvas.set_color_ansi(caca.COLOR_RED, caca.COLOR_BLUE)
+        else:
+            print "creating generic size canvas"
+            self.canvas = Canvas(0, 0)
+            self.canvas.set_color_ansi(caca.COLOR_RED, caca.COLOR_BLUE)
+        self.border_builder(spec)
+        if "text" in spec.keys():
+            print "drawing text: " + spec["text"]
+            self.canvas.put_str(1, 1, spec["text"])
+
+
 config = json.load(open("tui.json"))
+display = TartanDisplay()
+widget = Widget(config)
 
-cv = Canvas(0, 0)
-dp = Display(cv)
-dp.set_driver('gl')
-
-
-# cv.put_str(0, 0, config["text"])
-# dp.get_event(caca.EVENT_KEY_PRESS, Event(), 99999999)
-# dp.refresh()
-
-def parse(tui):
-    if "width" in tui.keys() and "height" in tui.keys():
-        print "creating canvas of specified size"
-        canvas = Canvas(tui["width"],
-                        tui["height"])
-        canvas.set_color_ansi(caca.COLOR_RED, caca.COLOR_BLUE)
-    else:
-        print "creating generic size canvas"
-        canvas = Canvas(0, 0)
-        canvas.set_color_ansi(caca.COLOR_RED, caca.COLOR_BLUE)
-    if "border" in tui.keys() and tui["border"] == True:
-        print "drawing border"
-        canvas.draw_box(0, 0, tui["width"],
-                        tui["height"],"X")
-    if "text" in tui.keys():
-        print "drawing text: " + tui["text"]
-        canvas.put_str(0, 0, tui["text"])
-    return canvas
-
-widget = parse(config)
-print "Widget: " + str(widget)
-cv.blit(0, 0, widget)
-dp.refresh()
-dp.get_event(caca.EVENT_KEY_PRESS, Event(), 99999999)
+display.canvas.blit(0, 0, widget.canvas)
+display.refresh()
+display.display.get_event(caca.EVENT_KEY_PRESS, Event(), 99999999)
