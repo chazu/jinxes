7f8266e8626af47234a017d7c70a6f1869d94ce7
Chaz Straney <chaz@jackrussellsoftware.com>
2013-06-08 14:35:10 -0400

    use specifies everywhere, add spec as class attribute, add text buffer builder, add draw method

--- engine.py
+++ engine.py
@@ -19,28 +19,42 @@ class TartanDisplay(object):
 
 class Widget(object):
 
-    def specifies(self, spec, key, value=None):
+    def draw(self):
+            self.canvas.put_str(self.text_origin[0],
+                                self.text_origin[1],
+                                self.text_buffer)
+
+    def specifies(self, key, value=None):
         """
         True if the spec doc has the key.
         If value is passed in, True only
         if key is present and equal to value
         """
-        return key in spec.keys() and (
-            spec[key] == value if value != None else True)
+        return key in self.spec.keys() and (
+            self.spec[key] == value if value != None else True)
 
-    def update(self):
-        pass
-
-    def border_builder(self, spec):
-        print str(spec["border"])
-        if self.specifies(spec, "border", True):
+    def border_builder(self):
+        if self.specifies("border", True):
             print "drawing border"
             self.border = True
-            self.canvas.draw_box(0, 0, spec["width"],
-                                 spec["height"],"X")
+            self.canvas.draw_box(0, 0, self.spec["width"],
+                                 self.spec["height"],"X")
+
+    def text_buffer_builder(self):
+        if self.specifies("text"):
+            self.text_buffer = self.spec["text"]
+        if self.specifies("border", True):
+            self.text_origin = (1, 1)
+        else:
+            self.text_origin = (0, 0)
 
     def __init__(self, spec):
-        if "width" in spec.keys() and "height" in spec.keys():
+        self.spec = spec
+        if self.specifies('name'):
+            self.name = spec["name"]
+        else:
+            self.name = "Unknown"
+        if self.specifies("height") and self.specifies("width"):
             print "creating canvas of specified size"
             self.canvas = Canvas(spec["width"],
                             spec["height"])
@@ -49,16 +63,16 @@ class Widget(object):
             print "creating generic size canvas"
             self.canvas = Canvas(0, 0)
             self.canvas.set_color_ansi(caca.COLOR_RED, caca.COLOR_BLUE)
-        self.border_builder(spec)
-        if "text" in spec.keys():
-            print "drawing text: " + spec["text"]
-            self.canvas.put_str(1, 1, spec["text"])
+        self.border_builder()
+        self.text_buffer_builder()
+
 
 
 config = json.load(open("tui.json"))
 display = TartanDisplay()
 widget = Widget(config)
 
+widget.draw()
 display.canvas.blit(0, 0, widget.canvas)
 display.refresh()
 display.display.get_event(caca.EVENT_KEY_PRESS, Event(), 99999999)
