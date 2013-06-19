b3aaa5f1d2796d22baeed76943c1f64957efb60a
Chaz Straney <chaz@jackrussellsoftware.com>
2013-06-18 23:14:28 -0400

    stub out class for local event dispatching

--- engine.py
+++ engine.py
@@ -7,11 +7,38 @@ from caca.display import Display, DisplayError, Event
 from display import TartanDisplay
 from widget import Widget
 
+class LocalEventDispatch:
+    """
+    Takes events from inside of the engine (caca events
+    such as keypresses), matches them to widgets
+    and executes callbacks which affect widgets.
+
+    Mapping of events to widgets is established at
+    instance initialization based on the specification
+    document used to construct the UI.
+    TODO: Since both this class and TartanDisplay use
+    the spec, pass it into both constructors
+    """
+
+    def __init__(self):
+        """
+        Parse the spec and create mappings to various
+        functions that will affect UI state
+        """
+        pass
+
+    def dispatch_events(self, events):
+        """
+        Do your magic: tell widgets what to do
+        """
+        pass
+
 
 class App:
 
     def __init__(self):
         self.display = TartanDisplay()
+        self.local_event_dispatch = LocalEventDispatch()
         self.digest_rate = 1000 #ms
         self.keypress_queue = []
         self.event_thing = Event()
