601d5abc8c1194ec28cbbe23c3cb3c038fa87b6e
Chaz Straney <chaz@jackrussellsoftware.com>
2013-07-21 17:05:23 -0400

    add graph proxy

---
+++ graph_proxy.py
@@ -0,0 +1,17 @@
+import networkx as nx
+
+class GraphProxy:
+
+    """
+    Wraps the directed graph data structure used to
+    represent connectivity between components
+    """
+
+    a
+
+
+
+    def __init__(self):
+        self.digraphs = []
+
+        
