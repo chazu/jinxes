629dd492ec0e4a9bc7e75809ed6afae91f73798b
Chaz Straney <chaz@jackrussellsoftware.com>
2013-08-19 00:13:37 -0400

    remote message dispatch now has a single exclusive queue by default

--- remote_event_dispatch.py
+++ remote_event_dispatch.py
@@ -1,4 +1,5 @@
 from time import sleep
+import uuid
 import pika
 
 class RemoteEventDispatch:
@@ -7,26 +8,37 @@ class RemoteEventDispatch:
     and passes them to the app
     """
 
+    def register_exclusive_queue(self):
+        self.channel.basic_publish(exchange='',
+                                   routing_key='register_player',
+                                   properties=pika.BasicProperties(
+                reply_to=self.exclusive_queue_id,
+                ),
+                                   body="json goes here")
+
+
+    def initialize_exclusive_queue(self):
+        self.default_queue = self.channel.queue_declare(exclusive=True)
+        self.exclusive_queue_id=self.default_queue.method.queue
+        self.register_exclusive_queue()
+
     def __init__(self, app):
 
         self.connection = connection = pika.BlockingConnection()
         self.channel = channel = connection.channel()
-        self.queue_name = queue
-        self.default_queue = self.channel.queue_declare(exclusive=True)
-        self.queues = []
         self.app = app
+        self.initialize_exclusive_queue()
 
-    def dispatch_message(self, message):
-        self.app.remote_messages.append(message)
-
-    def declare_queue(self, queue_name):
-        self.queues.append({"name": queue_name,
-                            "queue": self.channel.queue_declare(
-                                queue=queue_name)
-                            })
+    def dispatch_message(self, channel, frame, method, body):
+        print "dispatching message========================="
+        print frame
+        print method
+        print body
+        self.app.remote_messages.append(body)
 
     def init_consume(self):
-        self.channel.basic_consume(self.dispatch_message, self.queue_name, no_ack=True)
+        self.channel.basic_consume(self.dispatch_message, self.exclusive_queue_id, no_ack=True)
 
     def check_queue(self):
-        self.channel.consume(self.queue_name)
+        return self.channel.consume(self.exclusive_queue_id)
+
