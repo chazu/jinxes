dad1cf19a97a1aa80d1a2b5ced46a6f90d15a464
Chaz Straney <chaz@jackrussellsoftware.com>
2013-08-28 21:52:35 -0400

    modify logging in app-wide version of specifies

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ class App:
                     target[key] == value if value != None else True)
         except KeyError:
             self.logger.warn("Key error when requesting path " + \
-                str(path) + " for widget " + str(self))
+                                 str(path) + " for app")
             return False
 
     def __init__(self, filename=None):
